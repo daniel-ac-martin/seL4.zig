e0b1ae0176f5de7bcd1c976628580c6a68e036e2
Daniel A.C. Martin <github@daniel-martin.co.uk>
2023-08-12 18:35:20 +0100

    Debug function for printing out boot_info

--- bootinfo.zig
+++ bootinfo.zig
@@ -1,4 +1,5 @@
 const seL4 = @import("./common.zig");
+const debugPrint = @import("./syscalls.zig").debugPrint;
 
 const Caps = enum(u8) {
     capNull = 0,
@@ -64,4 +65,48 @@ pub fn getBootInfo() *BootInfo {
 pub fn setBootInfo(boot_info: *BootInfo) void {
     __sel4_boot_info = boot_info;
     thread.setIPCBuffer(boot_info.*.ipcBuffer);
+}
+
+// Inspired by: https://github.com/seL4/seL4_libs/blob/master/libsel4debug/src/bootinfo.c
+pub fn debugPrintBootInfo(boot_info: *BootInfo) void {
+    debugPrint("Node {d} of {d}\n", .{ boot_info.*.nodeID, boot_info.*.numNodes });
+    debugPrint("IOPT levels:     {d}\n", .{boot_info.*.numIOPTLevels});
+    debugPrint("IPC buffer:      {}\n", .{boot_info.*.ipcBuffer});
+    debugPrint("\n", .{});
+    debugPrint("sharedFrames:    [{d} --> {d})\n", .{ boot_info.*.sharedFrames.start, boot_info.*.sharedFrames.end });
+    debugPrint("ioSpaceCaps:     [{d} --> {d})\n", .{ boot_info.*.ioSpaceCaps.start, boot_info.*.ioSpaceCaps.end });
+    debugPrint("schedcontrol:     [{d} --> {d})\n", .{ boot_info.*.schedcontrol.start, boot_info.*.schedcontrol.end });
+    debugPrint("userImagePaging: [{d} --> {d})\n", .{ boot_info.*.userImagePaging.start, boot_info.*.userImagePaging.end });
+    debugPrint("extraBIPages:     [{d} --> {d})\n", .{ boot_info.*.extraBIPages.start, boot_info.*.extraBIPages.end });
+    debugPrint("userImageFrames: [{d} --> {d})\n", .{ boot_info.*.userImageFrames.start, boot_info.*.userImageFrames.end });
+    debugPrint("untypeds:        [{d} --> {d})\n", .{ boot_info.*.untyped.start, boot_info.*.untyped.end });
+    debugPrint("Empty slots:     [{d} --> {d})\n", .{ boot_info.*.empty.start, boot_info.*.empty.end });
+    debugPrint("\n", .{});
+    debugPrint("Initial thread domain: {d}\n", .{boot_info.*.initThreadDomain});
+    debugPrint("Initial thread cnode size: {d}\n", .{boot_info.*.initThreadCNodeSizeBits});
+    debugPrint("\n", .{});
+    debugPrint("List of untypeds\n", .{});
+    debugPrint("----------------\n", .{});
+    debugPrint("{s:3}: {s:12} | {s:4} | {s:6}\n", .{ "No", "Paddr", "Size", "Device" });
+    debugPrint("------------------| ---- | ------\n", .{});
+
+    const first_untyped = boot_info.*.untyped.start;
+    const last_untyped = boot_info.*.untyped.end;
+    const untyped_num = last_untyped - first_untyped;
+
+    var sizes = [_]u64{0} ** 256;
+
+    for (boot_info.*.untypedList[0..untyped_num], 0..) |v, i| {
+        const index = v.sizeBits;
+        sizes[index] += 1;
+        debugPrint("{:3}: {x:12} | {:4} | {d:6}\n", .{ i + first_untyped, boot_info.*.untypedList[i].paddr, v.sizeBits, v.isDevice });
+    }
+
+    debugPrint("Untyped summary\n", .{});
+
+    for (sizes, 0..) |v, i| {
+        if (v != 0) {
+            debugPrint("{d} untypeds of size {d}\n", .{ v, i });
+        }
+    }
 }
