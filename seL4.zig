4e516e61242e45794adc7109a229a94c6f335fc1
Daniel A.C. Martin <github@daniel-martin.co.uk>
2023-08-26 21:24:52 +0100

    Simple C import of libsel4

--- seL4.zig
+++ seL4.zig
@@ -1,8 +1,12 @@
-pub usingnamespace @import("./common.zig");
-pub usingnamespace @import("./bootinfo.zig");
-pub usingnamespace @import("./syscalls.zig");
+pub usingnamespace @cImport({
+    @cInclude("sel4/sel4.h");
+});
 
-pub const thread = @import("./thread.zig");
-pub const capabilities = @import("./capabilities.zig");
+// pub usingnamespace @import("./common.zig");
+// pub usingnamespace @import("./bootinfo.zig");
+// pub usingnamespace @import("./syscalls.zig");
+
+// pub const thread = @import("./thread.zig");
+// pub const capabilities = @import("./capabilities.zig");
 
 usingnamespace @import("./entrypoints.zig");
